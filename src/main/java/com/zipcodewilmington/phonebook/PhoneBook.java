6c35300c761ad5957518f3d95d41243745b495a5
Daniel M. Fuentes <dmf@pabar.org>
2018-02-14 15:33:49 -0500

    setup class blueprint

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -4,4 +4,28 @@ package com.zipcodewilmington.phonebook;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    //variables
+    private String name;
+    private String phoneNumber;
+
+    //lookup method
+    public static void lookup() {
+        //
+    }
+
+    //add method
+    public static void add() {
+        //
+    }
+
+    //remove method
+    public static void remove() {
+        //
+    }
+
+    //display() method
+    public static void display() {
+        //
+    }
 }
